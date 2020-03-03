9468775b1f45859c15513e00615314fb5ec27563
Scott <sacker@mail.bradley.edu>
2020-03-02 22:54:59 -0600

    Fixed a glitch or two

--- AlterHeart/Assets/Scripts/PlayerBehaviour.cs
+++ AlterHeart/Assets/Scripts/PlayerBehaviour.cs
@@ -46,7 +46,9 @@ public class PlayerBehaviour : MonoBehaviour
 
     private bool jumpingToWall = false; 
 
-    public BoxCollider boxCollider; // drag BoxCollider ref in editor
+    private BoxCollider boxCollider; // drag BoxCollider ref in editor
+
+    private bool onWall = false;
 
     private void Start()
     {
@@ -54,6 +56,7 @@ public class PlayerBehaviour : MonoBehaviour
         secondPhase = false;
         jumpForce = jumpForceDimension2;
 
+        boxCollider = GetComponent<BoxCollider>();
         myNormal = transform.up;
         distGround = boxCollider.size.y - boxCollider.center.y; // distance from transform.position to ground
     }
@@ -67,7 +70,9 @@ public class PlayerBehaviour : MonoBehaviour
 
     private void Update()
     {
-
+        onWall = myNormal != Vector3.up;
+        
+       
         if ((gameObject.transform.position.y <= -1.5f) && !secondPhase)
         {
             SceneManager.LoadScene("ProbuilderTest");
@@ -94,100 +99,7 @@ public class PlayerBehaviour : MonoBehaviour
         
     }
 
-    void WallWalking()
-    {
-        if (jumpingToWall) return; // don't do any of this while jumping to a wall
-
-        Ray ray;
-        RaycastHit hit;
-
-        if (Input.GetButtonDown("Jump"))
-        { 
-            ray = new Ray(transform.position, transform.forward);
-            if (Physics.Raycast(ray, out hit, jumpRange))
-            { // wall ahead?
-                JumpToWall(hit.point, hit.normal); // yes: jump to the wall
-            }
-            else if (isGrounded)
-            { // no: if grounded, jump up
-                rb.velocity += jumpForce * myNormal;
-            }
-        }
-
-        // movement code - turn left/right with Horizontal axis:
-        
-        // update surface normal and isGrounded:
-        ray = new Ray(transform.position, -myNormal); // cast ray downwards
-
-        if (Physics.Raycast(ray, out hit))
-        { 
-            // use it to update myNormal and isGrounded
-            isGrounded = hit.distance <= distGround + deltaGround;
-            surfaceNormal = hit.normal;
-        }
-        else
-        {
-            isGrounded = false;
-            surfaceNormal = Vector3.up; // assume usual ground normal to avoid "falling forever"
-        }
-
-        myNormal = Vector3.Lerp(myNormal, surfaceNormal, lerpSpeed * Time.deltaTime);
-        
-        // find forward direction with new myNormal:
-        Vector3 myForward = Vector3.Cross(transform.right, myNormal);
-        
-        // align character to the new myNormal while keeping the forward direction:
-        Quaternion targetRot = Quaternion.LookRotation(myForward, myNormal);
-        transform.rotation = Quaternion.Lerp(transform.rotation, targetRot, lerpSpeed * Time.deltaTime);
-
-        // move the character forth/back with Vertical axis:
-        transform.Translate(0, 0, Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
-    }
-
-    /// <summary>
-    /// Determines what transform variables are needed to move the player to the position to which they want to move
-    /// </summary>
-    /// <param name="point">The exact point that you will jump to on the wall</param>
-    /// <param name="normal">The player's current idea of "up"</param>
-    private void JumpToWall(Vector3 point, Vector3 normal)
-    {
-        // jump to wall
-        jumpingToWall = true; 
-        rb.isKinematic = true; 
-
-        Vector3 origPos = transform.position;
-        Quaternion origRot = transform.rotation;
-
-        Vector3 dstPos = point + normal * (distGround + 0.5f); // will jump to 0.5 above wall
-        Vector3 myForward = Vector3.Cross(transform.right, normal);
-        Quaternion dstRot = Quaternion.LookRotation(myForward, normal);
-
-        StartCoroutine(jumpTime(origPos, origRot, dstPos, dstRot, normal));
-    }
-
-    /// <summary>
-    /// Slowly moves player to new position
-    /// </summary>
-    /// <param name="origPos">The player's original position</param>
-    /// <param name="origRot">The player's original rotation</param>
-    /// <param name="newPos">The player's new position</param>
-    /// <param name="newRot">The player's new rotation</param>
-    /// <param name="normal">Which direction is "up" for the player</param>
-    /// <returns></returns>
-    private IEnumerator jumpTime(Vector3 origPos, Quaternion origRot, Vector3 newPos, Quaternion newRot, Vector3 normal)
-    {
-        for (float t = 0.0f; t < 1.0f;)
-        {
-            t += Time.deltaTime;
-            transform.position = Vector3.Lerp(origPos, newPos, t);
-            transform.rotation = Quaternion.Slerp(origRot, newRot, t);
-            yield return null; // return here next frame
-        }
-        myNormal = normal; // update myNormal
-        rb.isKinematic = false; // enable physics
-        jumpingToWall = false; // jumping to wall finished
-    }
-
+    
     private void NormMovement()
     {
         input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
@@ -215,6 +127,110 @@ public class PlayerBehaviour : MonoBehaviour
         }
     }
 
+    void WallWalking()
+    {
+        if (jumpingToWall) return; // don't do any of this while jumping to a wall
+
+        Ray ray;
+        RaycastHit hit;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            ray = new Ray(transform.position, transform.forward);
+            if (Physics.Raycast(ray, out hit, jumpRange))
+            { // wall ahead?
+                JumpToWall(hit.point, hit.normal); // yes: jump to the wall
+            }
+            else if (isGrounded)
+            { // no: if grounded, jump up
+                rb.velocity += jumpForce * myNormal;
+            }
+        }
+
+        // movement code - turn left/right with Horizontal axis:
+
+        // update surface normal and isGrounded:
+        ray = new Ray(transform.position, -myNormal); // cast ray downwards
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            // use it to update myNormal and isGrounded
+            isGrounded = hit.distance <= distGround + deltaGround;
+            surfaceNormal = hit.normal;
+        }
+        else
+        {
+            isGrounded = false;
+            surfaceNormal = Vector3.up; // assume usual ground normal to avoid "falling forever"
+        }
+
+        myNormal = Vector3.Lerp(myNormal, surfaceNormal, lerpSpeed * Time.deltaTime);
+
+        // find forward direction with new myNormal:
+        Vector3 myForward = Vector3.Cross(transform.right, myNormal);
+
+        // align character to the new myNormal while keeping the forward direction:
+        Quaternion targetRot = Quaternion.LookRotation(myForward, myNormal);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRot, lerpSpeed * Time.deltaTime);
+
+        // move the character forth/back with Vertical axis:
+        transform.Translate(0, 0, Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// Determines what transform variables are needed to move the player to the position to which they want to move
+    /// </summary>
+    /// <param name="point">The exact point that you will jump to on the wall</param>
+    /// <param name="normal">The player's current idea of "up"</param>
+    private void JumpToWall(Vector3 point, Vector3 normal)
+    {
+        // jump to wall
+        jumpingToWall = true;
+        rb.isKinematic = true;
+
+        Vector3 origPos = transform.position;
+        Quaternion origRot = transform.rotation;
+
+        Vector3 newPos = point + normal * (distGround + 0.5f); // will jump to 0.5 above wall
+        Vector3 myForward = Vector3.Cross(transform.right, normal);
+        Quaternion newRot = Quaternion.LookRotation(myForward, normal);
+
+        StartCoroutine(jumpTime(origPos, origRot, newPos, newRot, normal));
+    }
+
+    /// <summary>
+    /// Slowly moves player to new position
+    /// </summary>
+    /// <param name="origPos">The player's original position</param>
+    /// <param name="origRot">The player's original rotation</param>
+    /// <param name="newPos">The player's new position</param>
+    /// <param name="newRot">The player's new rotation</param>
+    /// <param name="normal">Which direction is "up" for the player</param>
+    /// <returns></returns>
+    private IEnumerator jumpTime(Vector3 origPos, Quaternion origRot, Vector3 newPos, Quaternion newRot, Vector3 normal)
+    {
+        for (float t = 0.0f; t < 1.0f;)
+        {
+            t += Time.deltaTime;
+            transform.position = Vector3.Lerp(origPos, newPos, t);
+            transform.rotation = Quaternion.Slerp(origRot, newRot, t);
+            yield return null; // return here next frame
+        }
+        myNormal = normal; // update myNormal
+        rb.isKinematic = false; // enable physics
+        jumpingToWall = false; // jumping to wall finished
+    }
+
+    /// <summary>
+    /// Causes player to fall back to the floor
+    /// </summary>
+    void BackToFloor()
+    {
+        Vector3 floorNormal = Vector3.up;
+
+        //set normal back to floor
+        //rotate character to proper floor position
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<GravityChanger>() != null)
