024c6d8274a79b6b0d3f327ec4b5563211330b42
probopastor <43503592+probopastor@users.noreply.github.com>
2020-03-08 18:20:41 -0500

    Revert "Lighting"
    This reverts commit 78de208fad174bce06ac314db4fa79d18ba9744c.

--- AlterHeart/Assets/Scripts/Objects/LightFlicker.cs
+++ AlterHeart/Assets/Scripts/Objects/LightFlicker.cs
@@ -9,40 +9,13 @@ using UnityEngine;
 
 public class LightFlicker : MonoBehaviour
 {
-    private Light thisLight;
-
-    public float flickerDuration = 2f;
-    public float minIntensity = 2f;
-    public float maxIntensity = 5f;
-
-    public float intensityModifier = 5f;
-
     void Start()
     {
-        thisLight = gameObject.GetComponent<Light>();
+        
     }
 
     void Update()
     {
-        PulsingLight();
-    }
-
-    private void PulsingLight()
-    {
-        float phi = (Time.time / flickerDuration) * 2 * Mathf.PI;
-        float amplitude = Mathf.Cos(phi) * 0.5f + 0.5f;
-        float newIntensity = amplitude * intensityModifier;
-
-        if (newIntensity > maxIntensity)
-        {
-            newIntensity = maxIntensity;
-        }
-
-        if (newIntensity < minIntensity)
-        {
-            newIntensity = minIntensity;
-        }
-
-        thisLight.intensity = newIntensity;
+        
     }
 }
