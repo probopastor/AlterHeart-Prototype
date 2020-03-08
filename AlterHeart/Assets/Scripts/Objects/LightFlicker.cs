e62028094d4f489d10e193631c84e8a4c9b64544
Scott <sacker@mail.bradley.edu>
2020-03-08 17:39:11 -0500

    Light Flicker Script + Visible T_points

---
+++ AlterHeart/Assets/Scripts/Objects/LightFlicker.cs
@@ -0,0 +1,21 @@
+﻿/*****************************************************************************
+// File Name: LightFlicker.cs
+// Author:
+// Creation Date: 
+//
+// Brief Description:
+*****************************************************************************/
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
